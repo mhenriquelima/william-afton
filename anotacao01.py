bb61ab238865756258ffb87e37a671950cc09c9e
mhenriquelima <mhenriquedelima1@gmail.com>
2025-02-24 09:38:07 -0300

    Create anotacao01.py

---
+++ anotacao01.py
@@ -0,0 +1,38 @@
+#email: jcmelo@unipe.edu.br
+
+#objetivo: 
+    #estudo dos aspectos de desenvolvimento de programas;
+    #conhecer e empregar linguagens de programação;
+    #converter algoritmos para uma linguagem de programação de alto nivel;
+    #desenvolver programas;
+    #entender tipos e estruturas de logica de programação;
+
+#metodologia
+    #aulas teoricas
+    #atividades praticas
+    #projetos
+    #discussoes e revisoes
+    #avaliacoes
+
+#recursos didaticos
+    #material de apoio: slides, apostilas, caderno e links para documentacao oficial;
+    #ferramentas de desenvolvimento: vs code, python, git;
+    #plataformas online: canva, figma, replit, youtube, github, etc;
+
+#pontos extra
+    #atividades extras poderão ser disbonibilizads e algumas poderão conceder pontos adcionais
+
+#datas
+    #12/03 = nome e rgm da dupla
+    #28/04 = avaliacao convencional
+    #14/04 = vistos no caderno
+    #22/04 = lista de exercicio beecrowd
+    #16/06 = avaliacao convencional
+    #12/06 = entrega do repositorio e video
+
+#livros
+    #curso intensivo de python
+    #use a cabeça! programacao
+    #aprendendo logica de programacao e algoritmos
+    
+#pdfdrive.com
